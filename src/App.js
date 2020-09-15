a699b24f8172a181fbf6dff4520669f3d6fff649
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-15 14:43:47 +0200

    Refactor input handling to use state variables

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, { useRef } from "react"
+import React, { useRef, useState } from "react"
 
 const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/"
 const LOGIN_URL = `${ROOT_URL}auth/api-token-auth/`
 
 function App() {
-	const emailInput = useRef(null)
-	const passwordInput = useRef(null)
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
 
 	function login() {
-		const email = emailInput.current.value
-		const password = passwordInput.current.value
 		console.log(email, password)
 		fetch(LOGIN_URL)
 	}
@@ -19,11 +17,21 @@ function App() {
 			<h1>The Event App</h1>
 			<div>
 				<label htmlFor="email">Email:</label>
-				<input ref={emailInput} name="email"></input>
+				<input
+					name="email"
+					type="email"
+					value={email}
+					onChange={(event) => setEmail(event.currentTarget.value)}
+				></input>
 			</div>
 			<div>
 				<label htmlFor="password">Password:</label>
-				<input ref={passwordInput} name="password" type="password"></input>
+				<input
+					name="password"
+					type="password"
+					value={password}
+					onChange={(event) => setPassword(event.currentTarget.value)}
+				></input>
 			</div>
 			<button onClick={login}>Login</button>
 		</div>
