2714108b879e831c8412ac7b92a45aefeec0e933
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-15 12:56:59 +0200

    Add login form and login button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react"
 
 function App() {
-	return <div></div>
+	function login() {
+		console.log("login-funktionen körs")
+	}
+	return (
+		<div>
+			<h1>The Event App</h1>
+			<div>
+				<label htmlFor="email">Email:</label>
+				<input name="email"></input>
+			</div>
+			<div>
+				<label htmlFor="password">Password:</label>
+				<input name="password" type="password"></input>
+			</div>
+			<button onClick={login}>Login</button>
+		</div>
+	)
 }
 
 export default App
