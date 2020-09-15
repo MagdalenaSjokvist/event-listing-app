89c9114226e77feb937f46a0badd2835952375e4
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-15 17:08:11 +0200

    Add EventDetailPage.jsx

---
+++ src/pages/EventDetailPage.jsx
@@ -0,0 +1,49 @@
+import React, { useContext, useEffect, useState } from "react"
+import { UserContext } from "../contexts/UserContext"
+
+const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/"
+
+export default function EventDetailPage(props) {
+	const { token } = useContext(UserContext)
+	const [mainPageData, setMainPageData] = useState(null)
+
+	useEffect(() => {
+		fetchEventMainPage()
+	}, [])
+
+	function fetchEventMainPage() {
+		const currentSlug = props.match.params.slug
+		const eventMainPageUrl = `${ROOT_URL}cms/${currentSlug}/main-page/`
+
+		fetch(eventMainPageUrl, {
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				setMainPageData(data)
+			})
+	}
+	return (
+		<div>
+			{/* Om det finns mainPageData för eventet - rendera rubrik, beskrivning */}
+			{mainPageData && (
+				<div>
+					<h1>{mainPageData.title}</h1>
+					<p>{mainPageData.description}</p>
+					{/* och om det finns - mappa igenom actionpoints, dvs. länkar vidare */}
+					{mainPageData.actionPoints &&
+						mainPageData.actionPoints.map((actionPoint, index) => {
+							return (
+								<div key={index}>
+									<p>{actionPoint.title}</p>
+								</div>
+							)
+						})}
+				</div>
+			)}
+		</div>
+	)
+}
