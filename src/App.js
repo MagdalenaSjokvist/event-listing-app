f3a1711e0cd8f602fb71757a4594c69caffb42c6
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-15 16:04:49 +0200

    Add UserContext.Provider with value={token, setToken}
    Wrap Switch-tags to make available everywhere in the app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { useRef, useState } from "react"
 import { Route, Switch } from "react-router-dom"
+import { UserContext } from "./contexts/UserContext"
 import EventListPage from "./pages/EventListPage"
 import LoginPage from "./pages/LoginPage"
 
 function App() {
+	const [token, setToken] = useState(null)
+
 	return (
 		<div>
-			<Switch>
-				<Route path="/event-list">
-					<EventListPage />
-				</Route>
-				<Route path="/">
-					<LoginPage />
-				</Route>
-			</Switch>
+			<UserContext.Provider value={{ token, setToken }}>
+				<Switch>
+					<Route path="/event-list">
+						<EventListPage />
+					</Route>
+					<Route path="/">
+						<LoginPage />
+					</Route>
+				</Switch>
+			</UserContext.Provider>
 		</div>
 	)
 }
