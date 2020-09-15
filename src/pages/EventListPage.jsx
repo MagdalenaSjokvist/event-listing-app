af3ab05177a3e1f6c85834958a53923bfc6d2c5a
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-15 16:23:44 +0200

    Set response to state variable and render event list

--- src/pages/EventListPage.jsx
+++ src/pages/EventListPage.jsx
@@ -1,11 +1,13 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { UserContext } from "../contexts/UserContext"
 
 const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/"
 const EVENT_LIST_URL = `${ROOT_URL}events/events/`
 
 export default function EventListPage() {
+	//Hämta {token} från useContext
 	const { token } = useContext(UserContext)
+	const [eventList, setEventList] = useState(null)
 
 	useEffect(() => {
 		fetchEventList()
@@ -18,12 +20,25 @@ export default function EventListPage() {
 				Authorization: `Bearer ${token}`,
 			},
 		})
+			//Sätt responsen till state-variabeln eventList med hjälp av setEventList
+			.then((res) => res.json())
+			.then((data) => {
+				setEventList(data.results)
+			})
 	}
 
 	return (
 		<div>
 			<h1>Event List Page</h1>
-			{token}
+			{/* Om det finns något i eventList - mappa igenom och rendera ut varje event */}
+			{eventList &&
+				eventList.map((eventItem) => {
+					return (
+						<div key={eventItem.id}>
+							<p>{eventItem.title}</p>
+						</div>
+					)
+				})}
 		</div>
 	)
 }
